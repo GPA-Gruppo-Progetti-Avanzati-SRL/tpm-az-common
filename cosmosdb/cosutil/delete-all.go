09d5212842e50cd8f4817a3b17f52b4ed47cd439
M.A.I <mario.imperato@gpagroup.it>
2023-04-17 01:19:35 +0200

    cosutil

---
+++ cosmosdb/cosutil/delete-all.go
@@ -0,0 +1,64 @@
+package cosutil
+
+import (
+	"context"
+	"github.com/Azure/azure-sdk-for-go/sdk/data/azcosmos"
+	"github.com/GPA-Gruppo-Progetti-Avanzati-SRL/tpm-az-common/cosmosdb/coslks"
+	"github.com/GPA-Gruppo-Progetti-Avanzati-SRL/tpm-common/util"
+	"github.com/rs/zerolog/log"
+)
+
+func DeleteAll(lks *coslks.LinkedService, collectionId, queryText string) (int, error) {
+	const semLogContext = "cos-util::delete-all"
+
+	cli, err := coslks.GetCosmosDbContainer("default", collectionId, false)
+	if err != nil {
+		log.Error().Err(err).Str("coll-id", collectionId).Str("query", queryText).Msg(semLogContext)
+		return 0, err
+	}
+
+	pr, err := NewPagedReader(lks, collectionId, queryText)
+	if err != nil {
+		log.Error().Err(err).Str("coll-id", collectionId).Str("query", queryText).Msg(semLogContext)
+		return 0, err
+	}
+
+	numberOfRowsDeleted := 0
+	rows, err := pr.Read()
+	if err != nil {
+		log.Error().Err(err).Str("coll-id", collectionId).Str("query", queryText).Msg(semLogContext)
+		return 0, err
+	}
+
+	logger := util.GeometricTraceLogger{}
+	hasNext := true
+	for hasNext {
+		for i, r := range rows {
+			logger.LogEvent(log.Trace().Str("row-id", r.Id).Int("num-row", i), semLogContext)
+			_, err = cli.DeleteItem(context.Background(), azcosmos.NewPartitionKeyString(r.PKey), r.Id, nil)
+			if err != nil {
+				np, nr := pr.Count()
+				log.Error().Err(err).Int("num-pages", np).Int("num-matches", nr).Int("num-dels", numberOfRowsDeleted).Str("coll-id", collectionId).Str("query", queryText).Msg(semLogContext)
+				return numberOfRowsDeleted, err
+			}
+			numberOfRowsDeleted++
+		}
+
+		if pr.HasNext() {
+			rows, err = pr.Read()
+		} else {
+			hasNext = false
+		}
+
+		if err != nil {
+			np, nr := pr.Count()
+			log.Error().Err(err).Int("num-pages", np).Int("num-matches", nr).Str("coll-id", collectionId).Str("query", queryText).Msg(semLogContext)
+			return numberOfRowsDeleted, err
+		}
+	}
+
+	np, nr := pr.Count()
+	log.Info().Err(err).Int("num-pages", np).Int("num-matches", nr).Int("num-dels", numberOfRowsDeleted).Str("coll-id", collectionId).Str("query", queryText).Msg(semLogContext)
+
+	return numberOfRowsDeleted, nil
+}
---
+++ cosmosdb/cosutil/read-paged.go
@@ -0,0 +1,73 @@
+package cosutil
+
+import (
+	"github.com/GPA-Gruppo-Progetti-Avanzati-SRL/tpm-az-common/cosmosdb/coslks"
+	"github.com/GPA-Gruppo-Progetti-Avanzati-SRL/tpm-az-common/cosmosdb/cosquery"
+	"github.com/GPA-Gruppo-Progetti-Avanzati-SRL/tpm-common/util"
+	"github.com/rs/zerolog/log"
+)
+
+type PagedReader struct {
+	qc         cosquery.QueryClient
+	pageNumber int
+	numReads   int
+	logger     util.GeometricTraceLogger
+}
+
+func (pr *PagedReader) Count() (int, int) {
+	return pr.pageNumber, pr.numReads
+}
+
+func (pr *PagedReader) HasNext() bool {
+	return pr.qc.HasNext()
+}
+
+func (pr *PagedReader) Read() ([]Document, error) {
+	const semLogContext = "page-reader::read"
+	var err error
+	var resp cosquery.Response
+
+	if pr.pageNumber <= 0 {
+		resp, err = pr.qc.Execute()
+	} else {
+		resp, err = pr.qc.Next()
+	}
+
+	if err != nil {
+		log.Error().Err(err).Msg(semLogContext)
+		return nil, err
+	}
+
+	var docs []Document
+	if resp.NumDocs() > 0 {
+		pr.pageNumber++
+		qr := resp.(*QueryResponse)
+		for i, d := range qr.Documents {
+			pr.numReads++
+			pr.logger.LogEvent(log.Trace().Int("num-reads", i).Int("page-number", pr.pageNumber), semLogContext)
+			docs = append(docs, d)
+		}
+	}
+
+	log.Info().Int("page-number", pr.pageNumber).Int("page-reads", len(docs)).Int("total-num-reads", pr.numReads).Msg(semLogContext)
+	return docs, nil
+}
+
+func NewPagedReader(lks *coslks.LinkedService, collectionId, queryText string) (*PagedReader, error) {
+	const semLogContext = "page-reade::new"
+
+	qc, err := cosquery.NewClientInstance(
+		cosquery.ResponseDecoderFunc(QueryResponseDecoderFunc),
+		cosquery.WithConnectionString(lks.ConnectionString()),
+		cosquery.WithDbName(lks.DbName()),
+		cosquery.WithCollectionName(lks.CollectionName("files")),
+		cosquery.WithQueryText(queryText),
+	)
+
+	if err != nil {
+		log.Error().Err(err).Str("coll-id", collectionId).Str("query", queryText).Msg(semLogContext)
+		return nil, err
+	}
+
+	return &PagedReader{qc: qc, logger: util.GeometricTraceLogger{}}, nil
+}
